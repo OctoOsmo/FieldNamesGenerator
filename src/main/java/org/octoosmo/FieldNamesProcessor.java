f37e06edcdba96a4b3e17b4fa10f21e50ed8bc8f
Aleksandr Tatarintsev <tatarincev_al@bk.ru>
2023-03-14 17:36:31 +0100

    Refactor FieldNameProcessor code to use var keyword for type inference in the process method. This makes the code more concise and easier to read.

--- src/main/java/org/octoosmo/FieldNamesProcessor.java
+++ src/main/java/org/octoosmo/FieldNamesProcessor.java
@@ -7,15 +7,11 @@ import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
-import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
-import javax.tools.JavaFileObject;
 import java.io.IOException;
-import java.io.Writer;
 import java.util.Collections;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -30,29 +26,29 @@ public class FieldNamesProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        for (TypeElement annotation : annotations) {
-            for (Element element : roundEnv.getElementsAnnotatedWith(annotation)) {
+        for (var annotation : annotations) {
+            for (var element : roundEnv.getElementsAnnotatedWith(annotation)) {
                 if (element.getKind() == ElementKind.CLASS) {
-                    String className = element.getSimpleName().toString();
-                    String packageName = processingEnv.getElementUtils().getPackageOf(element).toString();
-                    List<String> fieldNames = element.getEnclosedElements().stream()
+                    var className = element.getSimpleName().toString();
+                    var packageName = processingEnv.getElementUtils().getPackageOf(element).toString();
+                    var fieldNames = element.getEnclosedElements().stream()
                             .filter(e -> e.getKind() == ElementKind.FIELD)
                             .map(e -> "\"" + e.getSimpleName() + "\"")
                             .collect(Collectors.toList());
-                    String fileContent = String.format("""
-                                    package %s;
-                                    import java.util.List;
+                    var fileContent = String.format("""
+                                package %s;
+                                import java.util.List;
 
-                                    public class %sFieldNames {
-                                        public static final List<String> FIELD_NAMES = List.of(
-                                            %s
-                                        );
-                                    }""",
+                                public class %sFieldNames {
+                                    public static final List<String> FIELD_NAMES = List.of(
+                                        %s
+                                    );
+                                }""",
                             packageName, className, String.join(",\n        ", fieldNames));
-                    String fileName = className + "FieldNames";
+                    var fileName = className + "FieldNames";
                     try {
-                        JavaFileObject fileObject = processingEnv.getFiler().createSourceFile(packageName + "." + fileName);
-                        try (Writer writer = fileObject.openWriter()) {
+                        var fileObject = processingEnv.getFiler().createSourceFile(packageName + "." + fileName);
+                        try (var writer = fileObject.openWriter()) {
                             writer.write(fileContent);
                         }
                     } catch (IOException e) {
